256db06df987ce201a1ae7a62ecf42a03678eeae
apcs <apcs@mvhs-u528ap01.dcsdk12.org>
2013-10-10 08:51:01 -0600

    Added personal accounts and scanners for name and pin.

--- ATMRunner.java
+++ ATMRunner.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class ATMRunner {
 	public static void main(String[] args) {
+
+		ATM Bob = new ATM("Bob", 1234, 100);
+		ATM Alice = new ATM("Alice", 3333, 250);
+		ATM Jt = new ATM("JT", 0001, 999999999);
+
+
 		
 		System.out.println("Please enter your account name: ");
-		Scanner reader = new Scanner(System.in);
-		ATM.getname();
+		Scanner name = new Scanner(System.in);
+		ATM.getName = name.nextString();
+		
 
 		System.out.println("Please enter your PIN number: ");
-		ATM.pin();
+		Scanner pin = new Scanner(System.in);
+		ATM.getPin = pin.nextInt();
 
 
 	}
